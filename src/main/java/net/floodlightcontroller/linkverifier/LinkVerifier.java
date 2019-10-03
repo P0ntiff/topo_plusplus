07530477c5d2e9e4d0fcfccaa9f9189e8b0af8ad
James Ferreira <jameswferreira@gmail.com>
2019-10-03 13:09:58 +1000

    Moving HiddenPacket to nested class

--- src/main/java/net/floodlightcontroller/linkverifier/LinkVerifier.java
+++ src/main/java/net/floodlightcontroller/linkverifier/LinkVerifier.java
@@ -19,6 +19,7 @@ import org.openflow.protocol.*;
 import org.openflow.protocol.action.OFAction;
 import org.openflow.protocol.action.OFActionOutput;
 import org.openflow.util.HexString;
+import org.openflow.util.U16;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.floodlightcontroller.core.module.IFloodlightModule;
@@ -61,23 +62,20 @@ public class LinkVerifier implements IOFMessageListener, IFloodlightModule<IFloo
 				if(bsn == null || bsn.getPayload() == null) return Command.STOP;
 				if(bsn.getPayload() instanceof LLDP == false) return Command.CONTINUE;
 				(new InternalStatisticsGetter((LLDP) bsn.getPayload(), sw, (OFPacketIn) msg, cntx, floodlightProvider)).start();
-
+				return Command.STOP;
 			} else if(eth.getPayload() instanceof LLDP) {
 				(new InternalStatisticsGetter((LLDP) eth.getPayload(), sw, (OFPacketIn) msg, cntx, floodlightProvider)).start();
-
+				return Command.STOP;
 			} else if(eth.getPayload() instanceof IPv4) { /* IF IPV4, CHECK IF HIDDEN PACKET */
 				IPv4 packet = (IPv4) eth.getPayload();
 
 				if(packetMap.containsKey(packet.hashCode())) {
 					List<String> info = packetMap.remove(packet.hashCode());
 					log.info("A hidden packet has been returned to the controller");
-					//(new HiddenPacket(floodlightProvider, deviceMap, packetMap).start();
 				} else {
 					return Command.CONTINUE; //not a HP, so process normally
 				}
-
 			}
-			return Command.STOP;
 		}
 		return Command.CONTINUE;
 	}
@@ -104,6 +102,135 @@ public class LinkVerifier implements IOFMessageListener, IFloodlightModule<IFloo
 	//***************
 	// NESTED CLASSES
 	//***************
+
+	/** HiddenPacket thread to send HP to desired switches
+	 *
+	 **/
+	public class HiddenPacket extends Thread {
+		protected IFloodlightProviderService provider;
+
+		//host map, K = Switch/Port Pair, V = Host_IP
+		private Map<Integer, NodePortTuple> deviceMap;
+		private Map<Integer, List<String>> packetMap;
+		private int h1;
+		private int h2;
+
+
+		public HiddenPacket(IFloodlightProviderService provider,
+							Map<Integer, NodePortTuple> deviceMap,
+							Map<Integer, List<String>> packetMap,
+							int[] hostIPs) {
+			this.provider = provider;
+			this.deviceMap = deviceMap;
+			this.packetMap = packetMap;
+			this.h1 = hostIPs[0];
+			this.h1 = hostIPs[1];
+		}
+
+		public void run() {
+/*
+        IOFSwitch srcSw = provider.getSwitch((deviceMap.get(h1)).getNodeId());
+        IOFSwitch dstSw = provider.getSwitch((deviceMap.get(h2)).getNodeId());
+
+
+        if (srcSw == null || dstSw == null) {
+            log.warn("Switch on path is offline");
+            return; //switch is offline, do nothing
+        }
+
+        Ethernet eth = generate_payload();
+        OFPacketOut po = generate_packet_out(eth);
+        OFMessage flowMod = generate_flow_rule((IPv4)eth.getPayload());
+
+        try {
+            //INSTALL FLOW RULE IN END POINT
+            dstSw.write(flowMod, null);
+            dstSw.flush();
+
+            //SEND HIDDEN PACKET INTO SYSTEM
+
+            srcSw.write(po, null);
+            srcSw.flush();
+            log.info("HP SENT INTO SYSTEM {}", (eth.getPayload()).hashCode());
+            Thread.sleep(500);
+
+            if(packetMap.containsKey(eth.getPayload().hashCode())){
+                log.warn("HP WAS NOT RETURNED IN 500ms");
+            } else {
+                log.warn("HP WAS RETURNED IN 500ms");
+            }
+
+        } catch (Exception e) {
+            log.error("Cannot write probing message to SW " + srcSw.getStringId());
+        }
+*/
+
+			return;
+		}
+
+/*
+    public Ethernet generate_payload(){
+        //just PING for the moment, but should randomly select payload type
+
+        IPacket packet = new IPv4()
+                .setProtocol(IPv4.PROTOCOL_ICMP)
+                .setSourceAddress(deviceMap.get(startPoint))
+                .setDestinationAddress(deviceMap.get(endPoint))
+                .setPayload(new ICMP()
+                        .setIcmpType((byte) 8)
+                        .setIcmpCode((byte) 0)
+                        .setPayload(new Data(new byte[]
+                                {0x76, (byte) 0xf2, 0x0, 0x2, 0x1, 0x1, 0x1}))
+                );
+
+        //not sure how next hop works for controller
+        Ethernet eth = new Ethernet().setSourceMACAddress("aa:aa:aa:aa:aa:aa")
+                .setDestinationMACAddress("bb:bb:bb:bb:bb:bb")
+                .setEtherType(Ethernet.TYPE_IPv4);
+
+        eth.setPayload(packet);
+        return eth;
+    }*/
+
+		public OFPacketOut generate_packet_out(Ethernet eth){
+
+			OFPacketOut po = (OFPacketOut) provider.getOFMessageFactory().getMessage((OFType.PACKET_OUT));
+			byte[] data = eth.serialize();
+
+			List<OFAction> actions = new ArrayList<OFAction>();
+			actions.add(new OFActionOutput(OFPort.OFPP_CONTROLLER.getValue(),
+					(short)0xFFFF));
+
+			po.setBufferId(OFPacketOut.BUFFER_ID_NONE);
+			po.setInPort(OFPort.OFPP_NONE);
+			po.setPacketData(data);
+			po.setActions(actions);
+			po.setActionsLength((short) OFActionOutput.MINIMUM_LENGTH);
+
+			// set data and data length
+			po.setLengthU(OFPacketOut.MINIMUM_LENGTH + data.length + po.getActionsLength() );
+
+			return po;
+		}
+
+		public OFMessage generate_flow_rule(IPv4 packet){
+			//TODO this port should be changed when shortest route is figured out
+			OFMatch match = new OFMatch().loadFromPacket(packet.serialize(), OFPort.OFPP_ALL.getValue());
+
+			List<OFAction> actions = new ArrayList<>();
+			actions.add(new OFActionOutput(OFPort.OFPP_CONTROLLER.getValue()));
+
+			OFMessage flowMod = ((OFFlowMod) provider.getOFMessageFactory().getMessage(OFType.FLOW_MOD))
+					.setMatch(match).setCommand(OFFlowMod.OFPFC_ADD)
+					.setOutPort(OFPort.OFPP_NONE)
+					.setActions(actions)
+					.setHardTimeout((short)5) //timeout rule after 5 seconds
+					.setPriority((short)1).setLength(U16.t(OFFlowMod.MINIMUM_LENGTH));
+			return flowMod;
+		}
+
+	}
+
 
 	public class WebGetter extends Thread {
 		private final String ipAddress = "localhost";
@@ -183,8 +310,6 @@ public class LinkVerifier implements IOFMessageListener, IFloodlightModule<IFloo
 	}
 
 
-
-
 	public class DeviceInfo {
 
 		public String entityClass;
@@ -194,6 +319,7 @@ public class LinkVerifier implements IOFMessageListener, IFloodlightModule<IFloo
 		public Map<String, String>[] attachmentPoint;
 		public long lastSeen;
 		public String dhcpClientName;
+
 
 
 		public DeviceInfo() {
@@ -211,8 +337,6 @@ public class LinkVerifier implements IOFMessageListener, IFloodlightModule<IFloo
 
 		}
 	}
-
-
 
 
 
